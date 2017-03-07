351cb0934af9f0af7adb983692028428f2f6299a
XuTianZhu <493512132@qq.com>
2017-03-07 21:12:56 +0800

    I don't want to adjust the css of the btn today

--- Demo of web/Background/Practice/js/t001.js
+++ Demo of web/Background/Practice/js/t001.js
@@ -34,23 +34,27 @@ window.onload = function() {
   var bg2 = document.querySelector('#bg2');
   var btn = document.querySelector('#btn-switch');
 
-  console.log(height);
   console.log(bg1.style);
-  bg1.style.width = width + "px";
-  bg1.style.height = height + "px";
-  console.log(bg1.style.width);
-  bg1.style.backgroundImage = 'url(' + pattern.png() + ')';
-  bg2.style.width = width + "px";
-  bg2.style.height = height + "px";
-  bg2.style.backgroundImage = 'url(' + pattern2.png() + ')';
-  // console.log(pattern.png());
+  // bg1.style.width = width + "px";
+  // bg1.style.height = height + "px";
+  // bg1.style.backgroundImage = 'url(' + pattern.png() + ')';
+  // bg2.style.width = width + "px";
+  // bg2.style.height = height + "px";
+  // bg2.style.backgroundImage = 'url(' + pattern2.png() + ')';
+  var bgWidth = width + "px";
+  var bgHeight = height + "px";
+  var styleString = "width: " + bgWidth + "; height: " + bgHeight;
+  // 如何根据非数字序号确定相应 css 表?
+  document.styleSheets[0].addRule(".bg", styleString);
+  styleString = "background-image: " + 'url(' + pattern.png() + ')';
+  document.styleSheets[0].addRule("#bg1", styleString);
+  styleString = "background-image: " + 'url(' + pattern2.png() + ')';
+  document.styleSheets[0].addRule("#bg2", styleString);
+  console.log(bg1.style);
 
   btn.addEventListener('click', function() {
     var flag = Switch.get_flag();
-    // console.log(flag);
     if(!flag) {
-      // bg1.style.display = "none";
-      // bg2.style.display = "block";
       bg1.classList.toggle('switch');
       bg2.classList.toggle('switch');
       Switch.ss();
@@ -58,8 +62,6 @@ window.onload = function() {
     } else {
       bg1.classList.toggle('switch');
       bg2.classList.toggle('switch');
-      // bg1.style.display = "block";
-      // bg2.style.display = "none";
       Switch.ss();
       console.log("bg1 show");
     }
