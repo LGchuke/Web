7ba345749076c8eb8b3f36975e2e2283d7b823e3
XuTianZhu <493512132@qq.com>
2017-03-12 20:38:39 +0800

    commit the content before the dinner & let's finish the closure

--- Demo of web/Button/Practice/js/ripple.js
+++ Demo of web/Button/Practice/js/ripple.js
@@ -1,56 +1,86 @@
-/**
- * @init
- */
+var start_time = Date.now();
+
+/* ======== Main ======== */
+
+var buttonWidth = 172;
+var buttonHeight = 62;
+// 这边故意不用 Math.pow
+// var Radius = Math.sqrt((buttonWidth * buttonWidth)/4.0 + (buttonHeight * buttonHeight)/4.0);
+var RippleDuration = 10;
+var speed = 4;
+
+var buttons = document.getElementsByTagName('button');
+
+for (var i = 0; i < buttons.length; i++) {
+  console.log(buttons[i].event);
+  buttons[i].onclick = function(e) {
+    var offsetX = e.offsetX;
+    var offsetY = e.offsetY;
+    var RadiusX = (this.offsetWidth - offsetX > offsetX?this.offsetWidth - offsetX:offsetX);
+    var RadiusY = (this.offsetHeight - offsetY > offsetY?this.offsetHeight - offsetY:offsetY);
+    var maxRadius = Math.sqrt(Math.pow(RadiusX, 2) + Math.pow(RadiusY, 2));
+    // var RadiusX = Math.max(buttonWidth - x, x);
+    // var RadiusY = Math.max(buttonHeight - y, y);
+    // var maxRadius = Math.sqrt(Math.pow(RadiusX, 2) + Math.pow(RadiusY, 2));
+    // console.log(maxRadius);
+
+    var inner = document.createElement('div');
+    this.appendChild(inner);
+    var that = this;
+    inner.style.width = speed + 'px';
+    inner.style.height = speed + 'px';
+    inner.style.transform = 'scale(1)';
+    inner.style.left = (offsetX - inner.offsetWidth/2) + 'px';
+    inner.style.top = (offsetY - inner.offsetHeight/2) + 'px';
+    var interval = setInterval(function () {
+
+      inner.tmps = parseInt(inner.style.transform.slice(6).slice(0, -1)) + 1;
+      console.log(inner.tmps);
+      inner.style.transform = 'scale(' + inner.tmps + ')';
+
+      inner.style.opacity = (inner.offsetWidth * inner.tmps)/maxRadius/2 + 0.2;
+      if((inner.offsetWidth * inner.tmps) >= maxRadius*2 && (inner.offsetHeight * inner.tmps) >= maxRadius*2) {
+        clearInterval(interval);
+        that.removeChild(inner);
+      }
+    }, RippleDuration);
+  };
+}
+
+
 window.onload = function init(argument) {
-  var start_time = Date.now();
   console.log(argument);
-  /* ======== Main ======== */
 
-  var buttonWidth = 172;
-  var buttonHeight = 62;
-  // 这边故意不用 Math.pow
-  var Radius = Math.sqrt((buttonWidth * buttonWidth)/4.0 + (buttonHeight * buttonHeight)/4.0);
-  var buttons = document.getElementsByTagName('button'), speed = 4;
+  // var buttons = document.getElementsByTagName('button');
 
+  // for (var i = 0; i < buttons.length; i++) {
+  //   console.log(buttons[i].event);
+  //   // buttons[i].onclick = displayEffect(buttons[i].event);
 
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function (e) {
-      var x = e.offsetX;
-      var y = e.offsetY;
-      var left = x - Radius;
-      var top = y - Radius;
-      var RadiusX = Math.max(buttonWidth - x, x);
-      var RadiusY = Math.max(buttonHeight - y, y);
-      var maxRadius = Math.sqrt(Math.pow(RadiusX, 2) + Math.pow(RadiusY, 2));
-      console.log(maxRadius);
-      var inner = document.createElement('div');
-      this.appendChild(inner);
-      var interval = setInterval(function () {
-        inner.style.width = (inner.offsetWidth + speed) + 'px';
-        inner.style.height = (inner.offsetWidth + speed) + 'px';
-        inner.style.left = (x - inner.offsetWidth/2) + 'px';
-        inner.style.top = (y - inner.offsetHeight/2) + 'px';
-        inner.style.opacity = inner.offsetWidth/maxRadius/2;
-        if(inner.offsetWidth >= maxRadius*2 && inner.offsetHeight >= maxRadius*2) {
-          clearInterval(interval);
-        }
-      }, 10);
+    // buttons[i].onmouseup = function(e) {
+    //   var e = e || window.event,
+    //       this_ = this,
+    //       inner = this.children[0];
+    //   // console.log(inner);
+    //   if(inner != undefined) {
+    //     // console.log("yes");
+    //     var interval = setInterval(function () {
+    //       inner.style.opacity = inner.style.opacity - speed/this_.offsetWidth;
+    //       if(inner.style.opacity <= 0) {
+    //         this_.removeChild(inner);
+    //         clearInterval(interval);
+    //       }
+    //     }, RippleDuration);
+    //   } else {
+    //     // console.log("no");
+    //   }
 
-
-
-
-    };
-
-  }
-
-
-
-
-
-
-  /* ======== Main End ======== */
-  var stop_time = Date.now();
-  console.log("this script last: " + (stop_time - start_time) + "ms");
-  start_time = null;
-  stop_time = null;
+    // };
+  // }
 };
+
+/* ======== Main End ======== */
+var stop_time = Date.now();
+console.log("this script last: " + (stop_time - start_time) + "ms");
+start_time = null;
+stop_time = null;
