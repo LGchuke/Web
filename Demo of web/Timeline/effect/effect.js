e0b5bc67027a5e3b3fb4277ae960f57982c20786
XuTianZhu <493512132@qq.com>
2017-03-23 23:24:55 +0800

    fadeIn & fadeOut etc... see from QQ FE.

---
+++ Demo of web/Timeline/effect/effect.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+  var d1 = document.getElementById("d1");
+  var d2 = document.getElementById("d2");
+
+
+  d1.classList.toggle("fade-in");
+  Sleep(500);
+  d2.classList.toggle("fade-in");
+
+}
