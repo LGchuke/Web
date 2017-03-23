60b787e01fff88b9f61f769b1c03b1565375d49e
XuTianZhu <493512132@qq.com>
2017-03-23 20:25:37 +0800

    modify the big problem (such stupid methods) 一开始思路对的，但是写着写着就忘了做类名检测，导致重复添加

--- Demo of web/Timeline/demo004x/js/pure.js
+++ Demo of web/Timeline/demo004x/js/pure.js
@@ -14,35 +14,30 @@ window.onscroll = function() {
 function showBlocks(blocks, offset) {
   for (var i = 0; i < timelineBlocks.length; i++) {
     // console.log(timelineBlocks[i].offsetTop);
-    // console.log(timelineBlocks[i].offsetLeft);
-    // console.log(window.innerHeight * offset);
-    console.log(timelineBlocks[i].offsetTop - window.scrollY + window.innerHeight * offset);
-
+    console.log("i: " + i + " -- " + (timelineBlocks[i].offsetTop - (window.scrollY + window.innerHeight * offset)));
     if (timelineBlocks[i].offsetTop <= (window.scrollY + window.innerHeight * offset)) {
-      timelineBlocks[i].querySelector(".cd-timeline-img").className = removeClassString(timelineBlocks[i].querySelector(".cd-timeline-img").className, "is-hidden");
-      timelineBlocks[i].querySelector(".cd-timeline-img").className = addClassString(timelineBlocks[i].querySelector(".cd-timeline-img").className, "bounce-in");
-      timelineBlocks[i].querySelector(".cd-timeline-content").className = removeClassString(timelineBlocks[i].querySelector(".cd-timeline-content").className, "is-hidden");
-      timelineBlocks[i].querySelector(".cd-timeline-content").className = addClassString(timelineBlocks[i].querySelector(".cd-timeline-content").className, "bounce-in");
+      // timelineBlocks[i].querySelector(".cd-timeline-img").className = removeClassString(timelineBlocks[i].querySelector(".cd-timeline-img").className, "is-hidden");
+      // timelineBlocks[i].querySelector(".cd-timeline-img").className = addClassString(timelineBlocks[i].querySelector(".cd-timeline-img").className, "bounce-in");
+      timelineBlocks[i].querySelector(".cd-timeline-img").classList.remove("is-hidden");
+      timelineBlocks[i].querySelector(".cd-timeline-img").classList.add("bounce-in");
+      timelineBlocks[i].querySelector(".cd-timeline-content").classList.remove("is-hidden");
+      timelineBlocks[i].querySelector(".cd-timeline-content").classList.add("bounce-in");
     }
-
-    // console.log(timelineBlocks[i].querySelector(".cd-timeline-img").className.split(/\s+/).length);
-    // console.log(timelineBlocks[i].querySelector(".cd-timeline-img"));
-    // console.log(timelineBlocks[i].querySelector(".cd-timeline-content"));
   }
 }
 
 function hideBlocks(blocks, offset) {
   for (var i = 0; i < timelineBlocks.length; i++) {
     if (timelineBlocks[i].offsetTop > (window.scrollY + window.innerHeight * offset)) {
-      timelineBlocks[i].querySelector(".cd-timeline-img").className = addClassString(timelineBlocks[i].querySelector(".cd-timeline-img").className, "is-hidden");
-      timelineBlocks[i].querySelector(".cd-timeline-content").className = addClassString(timelineBlocks[i].querySelector(".cd-timeline-content").className, "is-hidden");
-      // timelineBlocks[i].className = addClassString(timelineBlocks[i].className, "is-hidden");
+      timelineBlocks[i].querySelector(".cd-timeline-img").classList.add("is-hidden");
+      timelineBlocks[i].querySelector(".cd-timeline-content").classList.add("is-hidden");
     }
   }
 }
 
 
 
+// such stupid methods
 function addClassString(ec, _class) {
   var classNames = ec.split(/\s+/);
   classNames[classNames.length] = _class;
