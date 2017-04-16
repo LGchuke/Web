e2021ad4196f5d51bab1a5f9b19d40355cfe1251
XuTianZhu <493512132@qq.com>
2017-04-16 13:09:07 +0800

    Add the wave effect. Next step is to add a ctl btn

--- demos/demo17.04.15/js/base.js
+++ demos/demo17.04.15/js/base.js
@@ -1,6 +1,6 @@
 var items = document.querySelectorAll('.et-item');
 
-(function() {
+var tx = (function() {
   var i = 0;
   for (let item of items) {
     // console.log(item);
@@ -9,4 +9,15 @@ var items = document.querySelectorAll('.et-item');
       item.classList.toggle('switch');
     }, 100 * i++);
   }
-}());
+});
+
+var wx = (function() {
+  const count = 20;
+  var i = 0;
+  while(i < count) {
+    setTimeout(() => {
+      tx();
+    }, 400 * i++);
+  }
+  console.log('end wx()');
+});
