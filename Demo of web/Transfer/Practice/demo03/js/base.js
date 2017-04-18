c3f7f1249055703c2347b4f84c39497b8e700310
XuTianZhu <493512132@qq.com>
2017-04-18 11:55:06 +0800

    demo03: to impl (arc -> circle)'s vision effect. Just make it looks like this for now

---
+++ Demo of web/Transfer/Practice/demo03/js/base.js
@@ -0,0 +1,62 @@
+var iCount = 36;
+var deg = 360 / parseFloat(iCount);
+var delay = 80;
+
+var etUl = document.querySelector('.et-wrap ul');
+var fragment = document.createDocumentFragment();
+var tmpLi = null;
+
+
+for(let i = 0; i < iCount; i++) {
+  tmpLi = document.createElement('li');
+  tmpLi.classList.add('et-item');
+
+  fragment.appendChild(tmpLi);
+}
+
+etUl.innerHTML = '';
+etUl.appendChild(fragment);
+
+var items = document.querySelectorAll('.et-item');
+
+
+var fFlag = false;
+function a(e, i) {
+  e.classList.toggle('switch');
+  setTimeout(() => {
+    e.style.transform = 'rotate(' + deg * i + 'deg)';
+  }, 100 * i++);
+}
+
+function b(e, i) {
+  e.classList.toggle('switch');
+  setTimeout(() => {
+    e.style.transform = 'rotate(0deg)';
+  }, 100 * i++);
+}
+
+var tx = (function() {
+  var i = 0;
+  if(fFlag) {
+    for (let item of items) {
+      b(item, i++);
+    }
+  } else {
+    for (let item of items) {
+      a(item, i++);
+    }
+  }
+  fFlag = !fFlag;
+
+});
+
+var wx = (function() {
+  const count = 20;
+  var i = 0;
+  while(i < count) {
+    setTimeout(() => {
+      tx();
+    }, 400 * i++);
+  }
+  console.log('end wx()');
+});
