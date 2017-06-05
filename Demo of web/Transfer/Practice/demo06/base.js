3b0e5cdc18fa864fd3efccc82136637ad1db4b55
XuTianZhu <493512132@qq.com>
2017-06-05 22:41:07 +0800

    Fix the bug: the redundancy of loop was ignored. Next step: add the reset, toggle buttons and events

--- Demo of web/Transfer/Practice/demo06/base.js
+++ Demo of web/Transfer/Practice/demo06/base.js
@@ -1,7 +1,8 @@
-var startPoint = 5;
-var randomRange = 8;
+var startPoint = 8;
+var randomRange = 5;
 var length = startPoint + Math.random() * randomRange;
 length = Math.ceil(length);
+// length = 11;
 
 // 创建并插入节点
 var fragment = document.createDocumentFragment();
@@ -29,8 +30,9 @@ var ar = function(l) {
   }
   return  t;
 }(length);
+// var ar = [4, 2, 3, 1, 7, 5, 6, 8, 9, 10, 11];
 
-
+console.log(ar);
 
 var items = document.querySelectorAll('.item');
 var h = 24;
@@ -62,33 +64,38 @@ for (let i = 0; i < ar.length; i++) {
 
 var i = 0;
 var j = i + 1;
+var t;
 var count = 1;
 var test = function() {
-  if ((i + 1) < ar.length) {
-    if ((j + 1) < ar.length) {
+  if ((i) < ar.length - 1) {
+    if ((j) < ar.length) {
       setTimeout(() => {
         if (ar[i] > ar[j]) {
-          var t = ar[i];
+          t = ar[i];
           ar[i] = ar[j];
           ar[j] = t;
           for (let k = 0; k < ar.length; k++) {
-            items[k].style.height = ar[k] * h + "px";
+            items[k].style.height = ar[k] * h + 'px';
           }
-
         }
+        console.log('count: ' + count++);
+        console.log('step: ' + i + '-' + j);
         j++;
-        console.log("step: " + i + '-' + j);
       }, delay);
     } else {
       i++;
       j = i + 1;
+      console.log('count: ' + count++); // 冗余计数
     }
+  } else {
+    console.log(count + '-- exit');
   }
-  console.log('count: ' + count++);
+
 };
 
-const n = length * (length - 1) / 2;
-console.log('n:' + n);
+console.log('l: ' + length);
+const n = length * (length + 1) / 2; // 要考虑到循环冗余
+console.log('n: ' + n);
 for (let c = 0; c < n; c++) {
   setTimeout(test, delay * c);
 }
