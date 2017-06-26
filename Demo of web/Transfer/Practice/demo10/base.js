a04bd70b20773a03ffae63ffcf0ce956556404e9
igoist <493512132@qq.com>
2017-06-26 22:34:08 +0800

    Effect complete. Still have some performance problem with render. Try JQ again

--- Demo of web/Transfer/Practice/demo10/base.js
+++ Demo of web/Transfer/Practice/demo10/base.js
@@ -6,26 +6,57 @@ console.log(contentWrap)
 var tabs = document.querySelectorAll('.et-music-tab')
 
 
+var delay = 320;
+var delayDelta = 10;
+
 var bindClick = function(i) {
   tabs[i].addEventListener('click', function(e) {
     // console.log(i)
     // console.log(e.target.dataset.id)
-    // console.log(document.querySelector('.et-music-tab.active'))
-    var preState = document.querySelector('.et-music-tab.active')
-    preState.classList.toggle('active')
-    preState = document.querySelector('.et-music-box-group.active')
-    preState.classList.toggle('active')
-    // console.log(preState.dataset.id)
-    // if (e.target.dataset.id > i) {
-    //   console.log(e.target.dataset.id + ': ' + i)
-    // } else {
-    //   console.log(e.target.dataset.id + ': ' + i)
-    // }
+    var preTab = document.querySelector('.et-music-tab.active')
+    preTab.classList.toggle('active')
+    var preState = preTab.dataset.id
+    newPart = document.querySelectorAll('.et-music-box-group')[i]
+    oldPart = document.querySelectorAll('.et-music-box-group')[preState]
 
 
-    document.querySelectorAll('.et-music-tab')[i].classList.toggle('active')
-    document.querySelectorAll('.et-music-box-group')[i].classList.toggle('active')
+    var preGroup = document.querySelector('.et-music-box-group.active')
+    if (preState > i) {
+      // console.log(preState + ' > ' + i + ' so left')
+      oldPart.classList.toggle('et-fadeOut-right')
+      setTimeout(function() {
+          oldPart.classList.toggle('active')
+      }, delay)
+      setTimeout(function() {
+          oldPart.classList.toggle('et-fadeOut-right')
+          newPart.classList.toggle('et-fadeIn-right')
+      }, delay + delayDelta)
+      setTimeout(function() {
+          newPart.classList.toggle('active')
+      }, delay + delayDelta * 2)
+      setTimeout(function() {
+          newPart.classList.toggle('et-fadeIn-right')
+      }, delay * 2 + delayDelta * 2)
+    } else {
+      // console.log(preState + ' < ' + i + ' so right')
+      oldPart.classList.toggle('et-fadeOut-left')
+      setTimeout(function() {
+          oldPart.classList.toggle('active')
+      }, delay)
+      setTimeout(function() {
+          oldPart.classList.toggle('et-fadeOut-left')
+          newPart.classList.toggle('et-fadeIn-left')
+      }, delay + delayDelta)
+      setTimeout(function() {
+          newPart.classList.toggle('active')
+      }, delay + delayDelta * 2)
+      setTimeout(function() {
+          newPart.classList.toggle('et-fadeIn-left')
+      }, delay * 2 + delayDelta * 2)
+    }
 
+    newTab = document.querySelectorAll('.et-music-tab')[i]
+    newTab.classList.toggle('active')
     contentWrap.classList = 'et-content-wrap trick' + i
   })
 }
