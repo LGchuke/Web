bc27afe834ee0c9632fb8898a6d1863210a4fd27
igoist <493512132@qq.com>
2017-06-26 21:59:12 +0800

    For switch effect: e.target.dataset.id is same to the bound(bind的过去分词😂 ) i. So I use the class .active to diff the state. Can switch directly now. In next version, add in effect(all with pure js)

--- Demo of web/Transfer/Practice/demo10/base.js
+++ Demo of web/Transfer/Practice/demo10/base.js
@@ -1,17 +1,34 @@
 // var tabList = document.querySelector('.et-music-tab-list');
-var contentWrap = document.querySelector('.et-content-wrap');
+var contentWrap = document.querySelector('.et-content-wrap')
 
-console.log(contentWrap);
+console.log(contentWrap)
 
-var tabs = document.querySelectorAll('.et-music-tab');
+var tabs = document.querySelectorAll('.et-music-tab')
 
 
 var bindClick = function(i) {
-  tabs[i].addEventListener('click', function() {
-    // console.log(i);
-    contentWrap.classList = 'et-content-wrap trick' + i;
-  });
-};
+  tabs[i].addEventListener('click', function(e) {
+    // console.log(i)
+    // console.log(e.target.dataset.id)
+    // console.log(document.querySelector('.et-music-tab.active'))
+    var preState = document.querySelector('.et-music-tab.active')
+    preState.classList.toggle('active')
+    preState = document.querySelector('.et-music-box-group.active')
+    preState.classList.toggle('active')
+    // console.log(preState.dataset.id)
+    // if (e.target.dataset.id > i) {
+    //   console.log(e.target.dataset.id + ': ' + i)
+    // } else {
+    //   console.log(e.target.dataset.id + ': ' + i)
+    // }
+
+
+    document.querySelectorAll('.et-music-tab')[i].classList.toggle('active')
+    document.querySelectorAll('.et-music-box-group')[i].classList.toggle('active')
+
+    contentWrap.classList = 'et-content-wrap trick' + i
+  })
+}
 
 for (var i = 0; i < tabs.length; i++) {
   bindClick(i);
