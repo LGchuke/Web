c2d2ed593606305cb09c44faa3216ce3c2bdc431
igoist <493512132@qq.com>
2017-06-27 23:57:31 +0800

    Refactor tabs switch with jQuery. Still the same problem, I'm thinking delayDelta may not enough for jQuery to operate DOM, or is due to setTimeout's really start、end delay

--- Demo of web/Transfer/Practice/demo11/base.js
+++ Demo of web/Transfer/Practice/demo11/base.js
@@ -1,56 +1,58 @@
-// var tabList = document.querySelector('.et-music-tab-list');
 var contentWrap = document.querySelector('.et-content-wrap')
 
-console.log(contentWrap)
-
 var tabs = document.querySelectorAll('.et-music-tab')
-
 
 var delay = 320;
 var delayDelta = 10;
 
 var bindClick = function(i) {
   tabs[i].addEventListener('click', function(e) {
-    // console.log(i)
     // console.log(e.target.dataset.id)
     var preTab = document.querySelector('.et-music-tab.active')
     preTab.classList.toggle('active')
     var preState = preTab.dataset.id
-    var newPart = document.querySelectorAll('.et-music-box-group')[i]
-    var oldPart = document.querySelectorAll('.et-music-box-group')[preState]
 
-    var preGroup = document.querySelector('.et-music-box-group.active')
+    // var newPart = document.querySelectorAll('.et-music-box-group')[i]
+    var newPart = $($('.et-music-box-group')[i])
+    var oldPart = $($('.et-music-box-group')[preState])
+
+    // console.log(newPart)
+    // console.log(oldPart)
+
+    // var preGroup = document.querySelector('.et-music-box-group.active')
     if (preState > i) {
       // console.log(preState + ' > ' + i + ' so left')
-      oldPart.classList.toggle('et-fadeOut-right') // add .et-fadeOut-right
+      // oldPart.classList.toggle('et-fadeOut-right') // add .et-fadeOut-right
+      oldPart.addClass('et-fadeOut-right')
       setTimeout(function() {
-          oldPart.classList.toggle('active') // remove .active
+          // oldPart.classList.toggle('active') // remove .active
+          oldPart.removeClass('active')
       }, delay)
       setTimeout(function() {
-          oldPart.classList.toggle('et-fadeOut-right') // remove .et-fadeOut-right for old part
-          newPart.classList.toggle('et-fadeIn-right') // add  .et-fadeIn-right for new part
+          oldPart.removeClass('et-fadeOut-right') // remove .et-fadeOut-right for old part
+          newPart.addClass('et-fadeIn-right') // add  .et-fadeIn-right for new part
       }, delay + delayDelta)
       setTimeout(function() {
-          newPart.classList.toggle('active') // add .active
+          newPart.addClass('active') // add .active
       }, delay + delayDelta * 2)
       setTimeout(function() {
-          newPart.classList.toggle('et-fadeIn-right') // remove .et-fadeIn-right
+          newPart.removeClass('et-fadeIn-right') // remove .et-fadeIn-right
       }, delay * 2 + delayDelta * 2)
     } else {
       // console.log(preState + ' < ' + i + ' so right')
-      oldPart.classList.toggle('et-fadeOut-left')
+      oldPart.addClass('et-fadeOut-left')
       setTimeout(function() {
-          oldPart.classList.toggle('active')
+          oldPart.removeClass('active')
       }, delay)
       setTimeout(function() {
-          oldPart.classList.toggle('et-fadeOut-left')
-          newPart.classList.toggle('et-fadeIn-left')
+          oldPart.removeClass('et-fadeOut-left')
+          newPart.addClass('et-fadeIn-left')
       }, delay + delayDelta)
       setTimeout(function() {
-          newPart.classList.toggle('active')
+          newPart.addClass('active')
       }, delay + delayDelta * 2)
       setTimeout(function() {
-          newPart.classList.toggle('et-fadeIn-left')
+          newPart.removeClass('et-fadeIn-left')
       }, delay * 2 + delayDelta * 2)
     }
 
