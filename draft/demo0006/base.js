51cb7610675ffb6ce26b4cf2b189cdea04aebbfe
igoist <493512132@qq.com>
2017-11-20 21:07:42 +0800

    Draft demo0006 -- 直播编程写游戏 01(part2)

--- draft/demo0006/base.js
+++ draft/demo0006/base.js
@@ -12,50 +12,167 @@ var Paddle = () => {
     image: image,
     speed: 5,
     x: 100,
-    y: 100
+    y: 150,
+    moveLeft() {
+      this.x -= this.speed
+    },
+    moveRight() {
+      this.x += this.speed
+    },
+    moveUp() {
+      this.y -= this.speed
+    },
+    moveDown() {
+      this.y += this.speed
+    },
+    collide(obj) {
+      if (obj.y + obj.image.height > this.y) {
+        if (obj.x > this.x && obj.x < this.x + this.image.width) {
+          log('collide')
+          return true
+        }
+      }
+      return false
+    }
   }
   return o
 }
 
+var Ball = () => {
+  var image = imageFromPath('./img/ball.png')
+  var o = {
+    image: image,
+    x: 100,
+    y: 100,
+    speedX: 8,
+    speedY: 8,
+    fired: true,
+    fire() {
 
-var __main = () => {
+    },
+    move() {
+      if (this.fired) {
+        if (this.x < 0 || this.x > 400) {
+          this.speedX = -this.speedX
+        }
+        if (this.y < 0 || this.y > 300) {
+          this.speedY = -this.speedY
+        }
+        this.x += this.speedX
+        this.y += this.speedY
+      }
+    },
+  }
+  return o
+}
+
+var GuaGame = function() {
+  g = {
+    actions: {},
+    keydowns: {},
+  }
   var canvas = document.getElementById('canvas')
   var context = canvas.getContext('2d')
 
-  var paddle = Paddle()
-  var leftDown = false
-  var rightDown = false
+  // draw
+  g.drawImage = (obj) => {
+    g.context.drawImage(obj.image, obj.x, obj.y)
+  }
 
+  // canvas 支持高分屏（High DPI）
+  if (window.devicePixelRatio) {
+    var hW = canvas.width;
+    var hH = canvas.height;
+
+    canvas.width = hW * window.devicePixelRatio;
+    canvas.height = hH * window.devicePixelRatio;
+    canvas.style.width = hW;
+    canvas.style.height = hH;
+    context.scale(window.devicePixelRatio, window.devicePixelRatio);
+  }
+
+  g.canvas = canvas
+  g.context = context
+
+  // events
   window.addEventListener('keydown', (e) => {
-    var k = e.key
-    log('keydown')
-    if (k === 'a' | k === 'A') {
-      leftDown = true
-    } else if (k === 'd' | k === 'D') {
-      rightDown = true
-    }
+    g.keydowns[e.key] = true
   })
 
   window.addEventListener('keyup', (e) => {
-    var k = e.key
-    log('keyup')
-    if (k === 'a' | k === 'A') {
-      leftDown = false
-    } else if (k === 'd' | k === 'D') {
-      rightDown = false
-    }
+    g.keydowns[e.key] = false
   })
 
+  g.registerAction = (key, callback) => {
+    g.actions[key] = callback
+  }
+
+  // timer
   setInterval(() => {
-    if (leftDown) {
-      paddle.x -= paddle.speed
+    // update
+    g.update()
+
+    var actions = Object.keys(g.actions)
+    for (var i = 0; i < actions.length; i++) {
+      var key = actions[i]
+      if (g.keydowns[key]) {
+        // 判断按键与执行回调
+        g.actions[key]()
+      }
     }
-    if (rightDown) {
-      paddle.x += paddle.speed
-    }
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    context.drawImage(paddle.image, paddle.x, paddle.y)
+
+    // clear
+    g.context.clearRect(0, 0, g.canvas.width, g.canvas.height)
+
+    // draw
+    g.draw()
   }, 1000 / 30)
+
+  return g
+}
+
+var __main = () => {
+  var game = GuaGame()
+  var paddle = Paddle()
+  var ball = Ball()
+
+  game.registerAction('a', () => {
+    paddle.moveLeft()
+  })
+  game.registerAction('A', () => {
+    paddle.moveLeft()
+  })
+  game.registerAction('d', () => {
+    paddle.moveRight()
+  })
+  game.registerAction('D', () => {
+    paddle.moveRight()
+  })
+  game.registerAction('w', () => {
+    paddle.moveUp()
+  })
+  game.registerAction('W', () => {
+    paddle.moveUp()
+  })
+  game.registerAction('s', () => {
+    paddle.moveDown()
+  })
+  game.registerAction('S', () => {
+    paddle.moveDown()
+  })
+
+  game.update = () => {
+    ball.move()
+
+    if (paddle.collide(ball)) {
+      ball.speedY *= -1
+    }
+  }
+
+  game.draw = () => {
+    game.drawImage(paddle)
+    game.drawImage(ball)
+  }
 }
 
 __main()
