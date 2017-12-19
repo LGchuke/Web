d17c1ea7cc986c39187a0fc8575295a915c9bd86
igoist <493512132@qq.com>
2017-12-19 17:02:04 +0800

    Draft demo0011 -- Gray Scale Blocks

--- draft/README.md
+++ draft/README.md
@@ -15,3 +15,4 @@
 * demo0005 -- Icon effect groups
 * demo0006 -- 萧井陌 B 站录播代码合集
 * demo0009 -- [Gameguide part of HeartStone Offical Homepage](http://hs.blizzard.cn/gameguide/)
+* demo0011 -- Gray Scale Blocks
---
+++ draft/demo0011/base.js
@@ -0,0 +1,23 @@
+let arr = [];
+
+let fragment = document.createDocumentFragment()
+
+let createColor = (v) => {
+  return 'rgb(' + v + ', ' + v + ', ' + v + ')'
+}
+
+for (let i = 0; i < 16; i++) {
+  let group = document.createElement('div')
+  group.classList.add('color-block-group')
+
+  for (let j = 0; j < 16; j++) {
+    let block = document.createElement('div')
+    block.classList.add('color-block')
+    let colorValue = i * j
+    block.style.background = createColor(colorValue)
+    group.appendChild(block)
+  }
+  fragment.appendChild(group)
+}
+
+document.querySelector('.box').appendChild(fragment)
