54959efd49d33d9df86f5106a2b3262fd63f47f2
igoist <493512132@qq.com>
2018-01-25 18:02:39 +0800

    Draft demo0016 -- Mock Quark: Navigation Switch(Update, use Promise to handle frequent click events)

--- draft/demo0016/base.js
+++ draft/demo0016/base.js
@@ -5,8 +5,32 @@ const mask = document.getElementById('mask')
 const c1 = document.getElementById('c1')
 const c2 = document.getElementById('c2')
 
+let duration = 360
+btn.flag = true
+
 btn.addEventListener('click', e => {
-  mask.classList.toggle('active')
-  c1.classList.toggle('inactive')
-  c2.classList.toggle('active')
+  /*
+   * Use Promise to handle frequent click
+   */
+  new Promise(resolve => {
+    if (btn.flag) {
+      btn.flag = false
+      mask.classList.toggle('active')
+      c1.classList.toggle('inactive')
+      c2.classList.toggle('active')
+      setTimeout(_ => {
+        resolve(true)
+      }, duration + 20)
+    } else {
+      resolve(false)
+    }
+  })
+  .then(unlocked => {
+    if (unlocked) {
+      btn.flag = true
+      log('Yeah! Actions complete')
+    } else {
+      log('Oops! Last group actions hasn\'t finish yet')
+    }
+  })
 })
