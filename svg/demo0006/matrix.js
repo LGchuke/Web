db09aafca448b1222073e5e416358d734c8d4f44
igoist <493512132@qq.com>
2018-04-15 23:50:35 +0800

    SVG demo0006 -- Custom SVG with JS(matrix upgrade, mouseover event, rect generation with color data)

--- svg/demo0006/matrix.js
+++ svg/demo0006/matrix.js
@@ -4,30 +4,31 @@ console.log(svgRoot);
 
 let obj = {
   matrix: [],
-  column: 11,
-  row: 6,
+  column: 11 * 3,
+  row: 6 * 3,
   size: 30
 };
 
-let handle = obj => {
+let handle = (obj, matrix) => {
   let tpl = '';
   const { column, row, size } = obj;
   const width = column * size;
   const height = row * size;
-  {/* let matrix = (new Array(row * column)).map((item, index) => { */}
-  {/*   console.log(item, index); */}
-  {/*   return index % 2; */}
-  {/* }); */}
-  let matrix = new Array(row * column);
-  for (let i = 0; i < matrix.length; i++) {
-    matrix[i] = i % 2;
-  }
+  // let matrix = (new Array(row * column)).map((item, index) => {
+  //   console.log(item, index);
+  //   return index % 2;
+  // });
+  {/* let matrix = new Array(row * column); */}
+  {/* for (let i = 0; i < matrix.length; i++) { */}
+  {/*   matrix[i] = i % 2; */}
+  {/* } */}
   
   obj.matrix = matrix;
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < column; j++) {
-      let color = matrix[i * column + j] ? '#ec414d' : '#0183ff';
-      tpl += `<rect x='${ j * size }' y='${ i * size } ' width='${ size }' height='${ size }' stroke='${ color }' fill='${ color }' stroke-width='1'/>`;
+      // let color = matrix[i * column + j] ? '#ec414d' : '#0183ff';
+      let color = matrix[1024 * 2.5 + i * column + j].hex;
+      tpl += `<rect x='${ j * size }' y='${ i * size }' width='${ size }' height='${ size }' stroke='${ color }' fill='${ color }' stroke-width='1'/>`;
     }
   }
   
@@ -36,5 +37,26 @@ let handle = obj => {
   svgRoot.innerHTML = tpl;
 };
 
-handle(obj);
-{/* console.log(obj.matrix); */}
+let xhr = new XMLHttpRequest();
+xhr.overrideMimeType('application/json');
+xhr.open('GET', './color.json');
+xhr.onload = () => {
+  if (xhr.status >= 200 && xhr.status < 300) {
+    const res = JSON.parse(xhr.response);
+    handle(obj, res);
+  }
+};
+xhr.send();
+// console.log(obj.matrix);
+
+let tmpRect;
+svgRoot.addEventListener('mouseover', e => {
+  e.target.style.fill = '#333';
+  e.target.style.stroke = '#333';
+  if (tmpRect) {
+    tmpRect.style.fill = '';
+    tmpRect.style.stroke = '';
+  };
+
+  tmpRect = e.target;
+});
